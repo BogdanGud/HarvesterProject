8bc187abcfd4d128cc4b1e8299b95e5c9e00cbdf
BogdanGud <GitHubpassqwe123>
2020-07-07 14:04:05 +0300

    create project, added dependencies

---
+++ src/main/java/com/bogdangud/harvesterproject/HarvesterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bogdangud.harvesterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvesterprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarvesterprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bogdangud/harvesterproject/HarvesterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdangud.harvesterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvesterprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
