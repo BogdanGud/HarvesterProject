1fa5df90bd275ccf27ef398e91b20abfc1495ffe
BogdanGud <GitHubpassqwe123>
2020-07-13 16:53:59 +0300

    added Revenue entity, table

---
+++ src/main/java/com/bogdangud/harvesterproject/model/Revenue.java
@@ -0,0 +1,35 @@
+package com.bogdangud.harvesterproject.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+public class Revenue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "square")
+    private double square;
+
+    @Column(name = "price_per_hectare")
+    private double pricePerHectare;
+
+    @Column(name = "combiner_driver_payment_hectare")
+    private double combineDriverPaymentPerHectare;
+
+    @Column(name = "comment")
+    private String comment;
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,27 @@
-CREATE TABLE user (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY name (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE user
+(
+    id   bigint(20)   NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY name (name)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-INSERT INTO user(name) VALUES ('Bogdan'), ('Michael'), ('Valera'), ('Nastya');
+CREATE TABLE revenue
+(
+    id                             bigint(20) NOT NULL AUTO_INCREMENT,
+    date                           date       NOT NULL,
+    square                         double     NOT NULL,
+    price_per_hectare                double     NOT NULL,
+    combiner_driver_payment_hectare double     NOT NULL,
+    comment                        varchar(2000),
+    PRIMARY KEY (id),
+    UNIQUE KEY name (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+INSERT INTO user(name)
+VALUES ('Bogdan'),
+       ('Michael'),
+       ('Valera'),
+       ('Nastya');
