04bcb41958bb25d870a7fc9d6a92860b46341851
BogdanGud <bogdan.gudzulyak@gmail.com>
2020-07-21 21:50:51 +0300

    expense_payments table test

---
+++ src/main/resources/db/migration/V4__payments_for_expense.sql
@@ -0,0 +1,13 @@
+CREATE TABLE expense_payments
+(
+    id         bigint(20) NOT NULL AUTO_INCREMENT,
+    amount     double     NOT NULL,
+    debtor_id  bigint(20) NOT NULL,
+    expense_id bigint(20) NOT NULL,
+    is_paid    boolean    NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (debtor_id) REFERENCES user (id),
+    FOREIGN KEY (expense_id) REFERENCES expense (id),
+    UNIQUE KEY name (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
